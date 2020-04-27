5daf8aae3a8960d0212a062b714a63cf4cbdf290
conanskyforce <conanskyforce@163.com>
2020-04-27 17:59:11 +0800

    feat: pack

---
+++ tiktik_downloader/pack.sh
@@ -0,0 +1,8 @@
+
+cd ../ 
+rm -rf tiktik_downloader*zip
+zip -r 'tiktik_downloader.zip' 'tiktik_downloader/' \ 
+-x 'tiktik_downloader/pack.sh' \
+-x 'tiktik_downloader/pack_ob.sh' \
+-x 'tiktik_downloader/background.js' -x 'tiktik_downloader/pack.sh'\
+-x 'tiktik_downloader/tik-tok_shots/*'
