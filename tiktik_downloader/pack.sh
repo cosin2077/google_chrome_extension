99861b0f5f9248e960b511a6f5e25130aa899ea8
conanskyforce <conanskyforce@163.com>
2020-04-27 18:00:37 +0800

    feat:backup

--- tiktik_downloader/pack.sh
+++ tiktik_downloader/pack.sh
@@ -4,5 +4,4 @@ rm -rf tiktik_downloader*zip
 zip -r 'tiktik_downloader.zip' 'tiktik_downloader/' \ 
 -x 'tiktik_downloader/pack.sh' \
 -x 'tiktik_downloader/pack_ob.sh' \
--x 'tiktik_downloader/background.js' -x 'tiktik_downloader/pack.sh'\
 -x 'tiktik_downloader/tik-tok_shots/*'
