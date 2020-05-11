7ff4be85e6d0976748f213850fe24c201815cc78
conanskyforce <conanskyforce@163.com>
2020-05-11 09:14:38 +0800

    feat: change pack

--- tiktik_downloader/pack.sh
+++ tiktik_downloader/pack.sh
@@ -1,7 +1,4 @@
 
 cd ../ 
 rm -rf tiktik_downloader*zip
-zip -r 'tiktik_downloader.zip' 'tiktik_downloader/' \ 
--x 'tiktik_downloader/pack.sh' \
--x 'tiktik_downloader/pack_ob.sh' \
--x 'tiktik_downloader/tik-tok_shots/*'
+zip -r 'tiktik_downloader.zip' 'tiktik_downloader/' -x 'tiktik_downloader/pack.sh' -x 'tiktik_downloader/pack_ob.sh' -x 'tiktik_downloader/tik-tok_shots/*'
