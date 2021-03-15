bf7133cf924117b3e629bca08650ed9da856ad32
conanskyforce <conanskyforce@163.com>
2021-03-15 15:23:35 +0800

    feat: v1.0

--- tiktik_downloader/writeLocales.js
+++ tiktik_downloader/writeLocales.js
@@ -1,9 +1,11 @@
 const fs = require('fs')
 const path = require('path')
 
-const tplName = 'Image downloader'
+const tplName = 'TickTick downloader'
 const tplDesc = 
-`Best image download tools for you! support all kinds of images, including background images！`
+`TickTick downloader,help browse and download your favorite videos`
+const dname = `TickTick downloader,help browse and download your favorite videos.`
+const ddesc = `Enjoy and download your favorite videos from tiktok.com`
 
 const msgFile = 'messages.json'
 const template = 
@@ -14,8 +16,8 @@ const template =
     "description": "$desc"
   },
   "appDesc": {
-    "message": "$name",
-    "description": "$desc"
+    "message": "$dname",
+    "description": "$ddesc"
   }
 }
 `
@@ -41,8 +43,9 @@ dirs.forEach(subDir => {
     let tmp = template.replace(/\$name/gi, langMap[subDir].name)
     data = tmp.replace(/\$desc/gi, langMap[subDir].desc)
   } else {
-    let tmp = template.replace(/\$name/gi, tplName)
-    data = tmp.replace(/\$desc/gi, tplDesc)
+    let tmp = template.replace(/\$name/gi, tplName).replace(/\$desc/gi, tplDesc)
+              .replace(/\$dname/gi, dname).replace(/\$ddesc/gi, ddesc)
+    data = tmp
   }
   fs.writeFileSync(resolve(root, subDir, msgFile), data)
   console.log('write succeed!')
