2867f5a89c052e3a89cb449f560bb69083dc0b0e
conanskyforce <conanskyforce@163.com>
2021-03-15 15:18:19 +0800

    feat: add tiktik

--- tiktik_downloader/pack.sh
+++ tiktik_downloader/pack.sh
@@ -1,4 +1,7 @@
 
 cd ../ 
 rm -rf tiktik_downloader*zip
-zip -r 'tiktik_downloader.zip' 'tiktik_downloader/' -x 'tiktik_downloader/pack.sh' -x 'tiktik_downloader/pack_ob.sh' -x 'tiktik_downloader/tik-tok_shots/*'
+zip -r 'tiktik_downloader.zip' 'tiktik_downloader/' \
+-x 'tiktik_downloader/pack.sh' \
+-x 'tiktik_downloader/pack_ob.sh' \
+-x 'tiktik_downloader/tik-tok_shots/*'
